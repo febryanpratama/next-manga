996f30272f0c56c809913adc31db586426becded
Febryan Pratama <febryancaesarpratama@gmail.com>
2024-09-13 02:22:26 +0700

    feat: adding get function list favorite

---
+++ src/app/(front)/favorite/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+
+import FavoriteView from "@/src/module/favorite/favoriteView";
+
+const page = () => {
+  return (
+    <>
+      <FavoriteView />
+    </>
+  );
+};
+
+export default page;
